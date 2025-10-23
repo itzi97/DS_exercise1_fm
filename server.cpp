28385e2c165fa3d380e8ed1f28bb06485361cd29
itzi97 <itzimr97@pm.me>
2025-10-23 12:49:13 +0200

    Get IP address·

--- server.cpp
+++ server.cpp
@@ -3,9 +3,38 @@
 #include <iostream>
 #include <thread>
 
+#include <arpa/inet.h> // this is for inet_ntoa
+#include <netdb.h>     // this is for gethostbyname
+#include <string>
+#include <unistd.h> // this is for gethostname
+
 using namespace std;
 
 int main(int argc, char **argv) {
+
+	// Get local IP
+
+	char host[256];
+
+	// to get local host name
+	if (gethostname(host, sizeof(host)) == -1) {
+		perror("[SERVER] gethostname failed");
+		return 1;
+	}
+
+	cout << "[SERVER] Host Name: " << host << endl;
+
+	// to get host information
+	hostent *he = gethostbyname(host);
+	if (he == nullptr) {
+		cerr << "[SERVER] gethostbyname failed" << endl;
+		return 1;
+	}
+
+	// to get and print the first IP address
+	in_addr *addr = (in_addr *)he->h_addr_list[0];
+	cout << "[SERVER] IP Address: " << inet_ntoa(*addr) << endl;
+
 	bool exit = false;
 
 	cout << "Server opening port 1067" << endl;
